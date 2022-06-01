95e948bd4ae8cafa9c50e3cbd3dafc9ae52bec19
LauBena <87872842+LauBena@users.noreply.github.com>
2022-06-01 13:40:37 -0300

    Agregamos el modelo Gender

---
+++ Models/Gender.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AppMovie.Models
+{
+    public class Gender 
+    {
+        [Key]
+        public int GenderID { get; set; }
+
+
+        [Display(Name = "Nombre del Genero")]
+        [Required(ErrorMessage = "Este valor es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El largo maximo es de {0} caracteres.")]
+        public string? GenderName { get; set; }
+    }
+}
