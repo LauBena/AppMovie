c8cf0fccf787a14a5fdef60aa2a10b90bf8a8697
LauBena <87872842+LauBena@users.noreply.github.com>
2022-08-24 19:46:51 -0300

    Agregamos la funcion eliminar movie y el boton correspondiente

--- wwwroot/js/rentalMovie.js
+++ wwwroot/js/rentalMovie.js
@@ -38,11 +38,32 @@ function SearchMovieTemp() {
                 $("#tableMovies").append(
                     "<tr>" +
                         "<th>" + item.movieName + "</th>" +
-                        "<th></th>" +
+                        "<th>" +                                            //juego de concatenacion
+                        "<button class='btn-create2' onclick='QuitarMovie(" + item.movieID + ");'>Eliminar</button>" +
+                        "</th>" +
                     "</tr>"
                 );
             });
 
+        },
+        error(result){
+            console.log(result);
+        }
+    })
+}
+
+function QuitarMovie(id){
+    console.log("Peli eliminada");
+    console.log(id);
+
+    $.ajax({
+        type: "POST",
+        url: "../../Rentals/QuitarMovie",
+        data: {MovieID: id},
+        success: function(resultado) {
+            if(resultado == true){
+                location.href="../../Rentals/Create";
+            }
         },
         error(result){
             console.log(result);
