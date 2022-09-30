2949ea1c97606fbc6fff993698eea2202fecfbde
LauBena <87872842+LauBena@users.noreply.github.com>
2022-09-30 19:25:09 -0300

    modelo return creado

---
+++ Migrations/20220930222304_ReturnModels.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppMovie.Migrations
+{
+    public partial class ReturnModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    CountryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.CountryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gender",
+                columns: table => new
+                {
+                    GenderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenderName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gender", x => x.GenderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producer",
+                columns: table => new
+                {
+                    ProducerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProducerName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producer", x => x.ProducerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RentalDetailTemp",
+                columns: table => new
+                {
+                    RentalDetailTempID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieID = table.Column<int>(type: "int", nullable: false),
+                    MovieName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentalDetailTemp", x => x.RentalDetailTempID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Return",
+                columns: table => new
+                {
+                    ReturnID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MovieName = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Return", x => x.ReturnID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Section",
+                columns: table => new
+                {
+                    SectionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SectionName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Section", x => x.SectionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    LocationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocationName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CountryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.LocationID);
+                    table.ForeignKey(
+                        name: "FK_Location_Country_CountryID",
+                        column: x => x.CountryID,
+                        principalTable: "Country",
+                        principalColumn: "CountryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MovieDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MovieData = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    SectionID = table.Column<int>(type: "int", nullable: false),
+                    GenderID = table.Column<int>(type: "int", nullable: false),
+                    ProducerID = table.Column<int>(type: "int", nullable: false),
+                    EstaAlquilada = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.MovieID);
+                    table.ForeignKey(
+                        name: "FK_Movie_Gender_GenderID",
+                        column: x => x.GenderID,
+                        principalTable: "Gender",
+                        principalColumn: "GenderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Movie_Producer_ProducerID",
+                        column: x => x.ProducerID,
+                        principalTable: "Producer",
+                        principalColumn: "ProducerID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Movie_Section_SectionID",
+                        column: x => x.SectionID,
+                        principalTable: "Section",
+                        principalColumn: "SectionID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partner",
+                columns: table => new
+                {
+                    PartnerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartnerName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    PartnerDirection = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    PartnerPhone = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    PartnerBirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LocationID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partner", x => x.PartnerID);
+                    table.ForeignKey(
+                        name: "FK_Partner_Location_LocationID",
+                        column: x => x.LocationID,
+                        principalTable: "Location",
+                        principalColumn: "LocationID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rental",
+                columns: table => new
+                {
+                    RentalID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RentalDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PartnerID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rental", x => x.RentalID);
+                    table.ForeignKey(
+                        name: "FK_Rental_Partner_PartnerID",
+                        column: x => x.PartnerID,
+                        principalTable: "Partner",
+                        principalColumn: "PartnerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RentalDetail",
+                columns: table => new
+                {
+                    RentalDetailID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RentalID = table.Column<int>(type: "int", nullable: false),
+                    MovieID = table.Column<int>(type: "int", nullable: false),
+                    MovieName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RentalDetail", x => x.RentalDetailID);
+                    table.ForeignKey(
+                        name: "FK_RentalDetail_Movie_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movie",
+                        principalColumn: "MovieID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RentalDetail_Rental_RentalID",
+                        column: x => x.RentalID,
+                        principalTable: "Rental",
+                        principalColumn: "RentalID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_CountryID",
+                table: "Location",
+                column: "CountryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_GenderID",
+                table: "Movie",
+                column: "GenderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_ProducerID",
+                table: "Movie",
+                column: "ProducerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_SectionID",
+                table: "Movie",
+                column: "SectionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partner_LocationID",
+                table: "Partner",
+                column: "LocationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_PartnerID",
+                table: "Rental",
+                column: "PartnerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalDetail_MovieID",
+                table: "RentalDetail",
+                column: "MovieID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RentalDetail_RentalID",
+                table: "RentalDetail",
+                column: "RentalID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RentalDetail");
+
+            migrationBuilder.DropTable(
+                name: "RentalDetailTemp");
+
+            migrationBuilder.DropTable(
+                name: "Return");
+
+            migrationBuilder.DropTable(
+                name: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "Rental");
+
+            migrationBuilder.DropTable(
+                name: "Gender");
+
+            migrationBuilder.DropTable(
+                name: "Producer");
+
+            migrationBuilder.DropTable(
+                name: "Section");
+
+            migrationBuilder.DropTable(
+                name: "Partner");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
