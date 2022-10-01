592b4c1ed47e77b8bfc043d0382589f6d56f04d3
LauBena <87872842+LauBena@users.noreply.github.com>
2022-10-01 00:10:30 -0300

    archivos eliminados

--- Data/AppMovieContext.cs
+++ Data/AppMovieContext.cs
@@ -31,8 +31,4 @@ using AppMovie.Models;
         public DbSet<AppMovie.Models.RentalDetail> RentalDetail { get; set; }
 
         public DbSet<AppMovie.Models.RentalDetailTemp> RentalDetailTemp { get; set; }
-
-        public DbSet<AppMovie.Models.Return> Return { get; set; }
-
-
     }
